d335715dfb07e8a25d2267bfc46f76e1fa5ae0c9
Careca Voador <e.rodrigo@outlook.com>
2022-07-15 16:31:42 -0300

    Adding readme.md

--- example.py
+++ example.py
@@ -6,22 +6,17 @@ MAX_WIDTH = 2592  # max page width (36 inches * 72 dpi = 2592 dots).
 MIN_HEIGHT = 792  # minimum page height (11 inches * 72 dpi = 792 dots).
 MARGIN = 72  # 1 inch (72 points) spacing between different items.
 
-SAMPLE_FILES = [Path("files/A3.pdf"), Path("files/A4.pdf"), Path("files/other.pdf")]
-
-
-def sort_height(pdf_file: Path) -> int:
-    """Returns mediabox height to sort files by height."""
-    _reader = PdfReader(open(pdf_file, "rb"))
-    return _reader.pages[0].mediabox.height
+SAMPLE_FILES = [
+    Path("sample_files/A3.pdf"),
+    Path("sample_files/A4.pdf"),
+    Path("sample_files/other.pdf"),
+]
 
 
 def main() -> None:
 
-    # Sort files from tallest to shortest.
-    sorted_files = sorted(SAMPLE_FILES, key=sort_height, reverse=True)
-
     # New file name where all the files will be merged.
-    merged_files = Path("files/merged.pdf")
+    merged_files = Path("sample_files/merged.pdf")
 
     with open(merged_files, "wb") as new_file:
         new_page = PageObject.create_blank_page(None, MAX_WIDTH, MIN_HEIGHT)
@@ -29,17 +24,17 @@ def main() -> None:
         # Last X position to use in horizontal axis translation.
         last_x = 0
 
-        for file in sorted_files:
+        for file in SAMPLE_FILES:
             reader = PdfReader(open(file, "rb"))
             page_to_merge = reader.pages[0]
 
-            # This works:
-            new_page.mergeTranslatedPage(page_to_merge, tx=last_x, ty=0, expand=True)
+            # This does NOT work:
+            op = Transformation().translate(tx=last_x, ty=0)
+            page_to_merge.add_transformation(op)
+            new_page.merge_page(page_to_merge, expand=True)
 
-            # This does not work:
-            # op = Transformation().translate(tx=last_x, ty=0)
-            # page_to_merge.add_transformation(op)
-            # new_page.merge_page(page_to_merge, expand=True)
+            # This WORKS:
+            # new_page.mergeTranslatedPage(page_to_merge, tx=last_x, ty=0, expand=True)
 
             last_x += page_to_merge.mediabox.width + MARGIN
 
---
+++ readme.md
@@ -0,0 +1,29 @@
+# Issue when trying to use transformations with PyPDF2.
+
+I'm currently working on a minimal application to merge several PDF files in a single big PDF page file. Sometimes I need to rotate it too.
+
+To do so, I'm currently using the `mergeTranslatedPage` method of the `PageObject` class. But, it says it's deprecated and I should "use `add_transformation` and `merge_page` instead".
+
+When I try to use the `add_transformation` method:
+
+(lines 32 to 35 of `example.py`)
+```python
+op = Transformation().translate(tx=last_x, ty=0)
+page_to_merge.add_transformation(op)
+new_page.merge_page(page_to_merge, expand=True)
+```
+
+The result is not what I was expecting:
+![Unexpected behavior](images/merged_not_ok.png)
+
+But if I look at the wireframe I can see something happens. I suspect the translation is occuring on the page objects, but not on the trimbox:
+
+![Wireframe view](images/merged_not_ok_wf.png)
+
+Although the `mergeTranslatedPage` method works as intended:
+
+![Expected behavior](images/merged_ok.png)
+
+So, what's the big deal? Why don't I just use `mergeTranslatedPage` then?
+
+Well, if I need to rotate some page before merging it to the big page, I never get the desired result whatever the method I use, be it `mergeRotatedTranslatedPage` or using the `translate` and `rotate` methods on the `Transformation` object.
